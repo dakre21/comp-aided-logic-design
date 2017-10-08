19fd243bdbf2f317cadb78a8317afa352f8dfb7f
David Akre <dakre21@gmail.com>
2017-10-07 18:06:22 -0600

    Breaking up input file into sub strings line by line

--- assignment_2/dakre_dpgen/src/HLSEngine.cpp
+++ assignment_2/dakre_dpgen/src/HLSEngine.cpp
@@ -9,11 +9,34 @@ HLSEngine::~HLSEngine() {
 }
 
 bool HLSEngine::parseBufferCreateVerilogSrc(char* buff, size_t buff_len) {
+    // Forward declarations 
+    char* sub_buff;
+    int buff_num = 0;
+    int k        = 0; 
+
+    // Create heap space for buffer and clear it
+    sub_buff = reinterpret_cast<char*>(malloc((buff_len + 1)*sizeof(char)));
+    memset(sub_buff, '\0', buff_len);
+
     // Parse buffer for behavioral netlist
     for (int i = 0; i < buff_len; i++) {
-        printf("%c", buff[i]);
-        // Write verilog code to new synthesizable verilog module
-        // Continue/Return based on successfullness
+        // Read buffer into substring buffer line by line
+        if (buff[i] == '\n') {
+            for (int j = buff_num; j < i; j++) {
+                sub_buff[k] = buff[j];
+                k++;
+            }
+
+            // TODO: Parse sub string for data path elements
+            printf("%s", sub_buff);
+
+            // Set loop elements
+            buff_num = i;
+            k = 0;
+
+            // Clear sub_buff
+            memset(sub_buff, '\0', buff_len);
+        } 
     }
     printf("\n");
     return true;
@@ -36,6 +59,7 @@ bool HLSEngine::createVerilogSrc(FILE* file_in, FILE* file_out) {
 
     // Create heap space for buffer
     buff = reinterpret_cast<char*>(malloc((buff_len + 1)*sizeof(char)));
+    memset(buff, '\0', buff_len);
 
     // Clear buffer
     rewind(file_in);
@@ -43,16 +67,19 @@ bool HLSEngine::createVerilogSrc(FILE* file_in, FILE* file_out) {
     // Read buffer
     fread(buff, buff_len, 1, file_in);
 
-    // NULL terminate buffer
-    buff[buff_len] = '\0'; 
-
     if (strlen(buff) != buff_len) {
         fprintf(stderr, "Failed to read all of the bytes in the file\n");
         return false;
     }
 
-    // Parse buffer and create verilog file
-    return parseBufferCreateVerilogSrc(buff, buff_len);
+    // Parse buffer and create verilog file and return result
+    if (parseBufferCreateVerilogSrc(buff, buff_len) != true) {
+        free(buff);
+        return false;
+    } else {
+        free(buff);
+        return true;
+    }
 }
 
 float HLSEngine::findCriticalPath(FILE* file_in, FILE* file_out) {
